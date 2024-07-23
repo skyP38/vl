e8d84c7ad9b2c29d3d8c771cb93d4f68d5701d35
skyP38 <81376635+skyP38@users.noreply.github.com>
2024-07-23 10:42:03 +0300

    fix loop

--- encoder8to3/encoder_param.v
+++ encoder8to3/encoder_param.v
@@ -6,12 +6,18 @@ module encoder_param #(
         output wire [OUT_W-1:0] out
 );
 
+wire [OUT_W-1:0] temp [IN_W-1:0];
+
 genvar Gi;
 generate for(Gi = 0; Gi < IN_W; Gi = Gi + 1)
 begin: loop
-	assign out = (in[Gi]) ? Gi : 0;
+    if (Gi == 0) 
+        assign temp[Gi] = 'h0;
+    else
+	    assign temp[Gi] = {OUT_W(in[Gi])} & Gi | temp[Gi-1];
 end
 endgenerate
 
+assign out = temp[IN_W - 1];
 
 endmodule
