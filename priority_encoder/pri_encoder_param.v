bb8fc21a56c90600396dbb3fb8f715b217bd57c9
skyP38 <81376635+skyP38@users.noreply.github.com>
2024-07-23 11:01:37 +0300

    fix multiple assign

--- priority_encoder/pri_encoder_param.v
+++ priority_encoder/pri_encoder_param.v
@@ -6,11 +6,18 @@ module pri_encoder #(
     output wire [OUT_W:0] out
 );
 
+wire [OUT_W-1:0] temp [IN_W-1:0];
+
 genvar Gi;
 generate for(Gi = 0; Gi < IN_W; Gi = Gi + 1)
 begin: loop
-	assign out = (in[Gi]) ? (IN_W-1 - Gi) : 0;
+    if (Gi == 0) 
+        assign temp[Gi] = 'h0;
+    else
+	    assign temp[Gi] = (in[Gi]) ? Gi : 0;
 end
 endgenerate
+
+assign out = temp[IN_W - 1];
 
 endmodule
